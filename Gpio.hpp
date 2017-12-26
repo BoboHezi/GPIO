13c7f5173997f650a465022086dc53c8494f7144
eliChang <eliflichang@gmail.com>
2017-12-26 17:46:47 +0800

    create repository

---
+++ Gpio.hpp
@@ -0,0 +1,32 @@
+#ifndef GPIO_H
+#define GPIO_H
+
+#include "stm32f1xx_hal.h"
+
+#define INPUT     GPIO_MODE_INPUT
+#define OUTPUT_PP GPIO_MODE_OUTPUT_PP
+#define OUTPUT_OD GPIO_MODE_OUTPUT_OD
+#define LOW       GPIO_PIN_RESET
+#define HIGH      GPIO_PIN_SET
+
+class Gpio
+{
+public:
+
+	Gpio(GPIO_TypeDef* group, uint16_t pin);
+
+	void setGroup(GPIO_TypeDef* group);
+	void setPin(uint16_t pin);
+	
+	void pinMode(int pinMode);
+	void digitalWrite(GPIO_PinState state);
+	int  digitalRead();
+
+private:
+	GPIO_TypeDef * group;
+	uint16_t pin;
+
+	void initPin();
+};
+
+#endif
