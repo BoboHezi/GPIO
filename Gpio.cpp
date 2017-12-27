2686805240bbe45dc3048ac0eef095c528f742a3
eliChang <eliflichang@gmail.com>
2017-12-27 09:29:36 +0800

    添加方法: digitalTogglePin()

--- Gpio.cpp
+++ Gpio.cpp
@@ -1,53 +1,53 @@
 #include "gpio.hpp"
 
 /*
- * 构造方法
- * @param group: 分组
- * @param pin: 引脚
- */
+* 构造方法
+* @param group: 分组
+* @param pin: 引脚
+*/
 Gpio::Gpio(GPIO_TypeDef* group, uint16_t pin) {
 	Gpio::group = group;
 	Gpio::pin = pin;
-
+	
 	initPin();
 }
 
 /*
- * 初始化引脚
- */
+* 初始化引脚
+*/
 void Gpio::initPin() {
 	/* GPIO Ports Clock Enable */
 	__HAL_RCC_GPIOC_CLK_ENABLE();
 	__HAL_RCC_GPIOD_CLK_ENABLE();
 	__HAL_RCC_GPIOA_CLK_ENABLE();
-
+	
 	/*Configure GPIO pin Output Level */
 	HAL_GPIO_WritePin(group, pin, GPIO_PIN_RESET);
 	pinMode(GPIO_MODE_OUTPUT_PP);
 }
 
 /*
- * 设置引脚分组
- * @param group: 分组
- */
+* 设置引脚分组
+* @param group: 分组
+*/
 void Gpio::setGroup(GPIO_TypeDef* group) {
 	Gpio::group = group;
 	initPin();
 }
 
 /*
- * 设置引脚
- * @param: 引脚
- */
+* 设置引脚
+* @param: 引脚
+*/
 void Gpio::setPin(uint16_t pin) {
 	Gpio::pin = pin;
 	initPin();
 }
 
 /*
- * 设置引脚模式
- * @param pinMode: 引脚模式(INPUT/OUTPUT)
- */
+* 设置引脚模式
+* @param pinMode: 引脚模式(INPUT/OUTPUT)
+*/
 void Gpio::pinMode(int pinMode) {
 	GPIO_InitTypeDef GPIO_InitStruct;
 	GPIO_InitStruct.Pin = pin;
@@ -57,9 +57,9 @@ void Gpio::pinMode(int pinMode) {
 }
 
 /*
- * 写引脚
- * @param state: 引脚状态(LOW/HIGH)
- */
+* 写引脚
+* @param state: 引脚状态(LOW/HIGH)
+*/
 void Gpio::digitalWrite(GPIO_PinState state) {
 	if (Gpio::group != 0 && Gpio::pin != 0) {
 		HAL_GPIO_WritePin(group, pin, state);
@@ -67,9 +67,16 @@ void Gpio::digitalWrite(GPIO_PinState state) {
 }
 
 /*
- * 读取引脚
- * @return: 引脚状态(LOW/HIGH)
- */
+* 读取引脚
+* @return: 引脚状态(LOW/HIGH)
+*/
 int Gpio::digitalRead() {
 	return HAL_GPIO_ReadPin(group, pin);
+}
+
+/**
+ * 切换引脚状态
+ */
+void Gpio::digitalTogglePin() {
+	HAL_GPIO_TogglePin(group, pin);
 }
--- Gpio.hpp
+++ Gpio.hpp
@@ -4,6 +4,7 @@
 #include "stm32f1xx_hal.h"
 
 #define INPUT     GPIO_MODE_INPUT
+#define OUTPUT    GPIO_MODE_OUTPUT_PP
 #define OUTPUT_PP GPIO_MODE_OUTPUT_PP
 #define OUTPUT_OD GPIO_MODE_OUTPUT_OD
 #define LOW       GPIO_PIN_RESET
@@ -12,20 +13,20 @@
 class Gpio
 {
 public:
-
+	
 	Gpio(GPIO_TypeDef* group, uint16_t pin);
-
+	
 	void setGroup(GPIO_TypeDef* group);
 	void setPin(uint16_t pin);
-	
 	void pinMode(int pinMode);
 	void digitalWrite(GPIO_PinState state);
 	int  digitalRead();
-
+	void digitalTogglePin();
+	
 private:
 	GPIO_TypeDef * group;
 	uint16_t pin;
-
+	
 	void initPin();
 };
 
